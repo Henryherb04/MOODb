3bae261ac474f44ede647843aab0107cb0ca06b4
herb <henryharris@seattleacademy.org>
2024-01-31 10:18:59 -0800

    MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOD

---
+++ script.js
@@ -0,0 +1,22 @@
+var colorTable = {
+    "angry": "red",
+    "sad": "blue",
+    "happy": "yellow",
+    "tired": "grey",
+    "scared": "purple",
+    "goofy": "pink",
+}
+
+
+function changeMood() {
+    const word = textBox.value;
+    if (word in colorTable) {
+        colorBox.style.backgroundColor = colorTable[word];
+    }
+    else {
+        colorBox.style.backgroundColor = "grey";
+    }
+}
+const textBox = document.getElementById('moodtext');
+textBox.addEventListener("input", changeMood);
+const colorBox = document.getElementById('moodbox');
